905cace46add0a6df7904ebeb7bf4acfa7d2c3cc
Sergi Muyo Abad <sergimuyo@gmail.com>
2022-02-21 17:13:15 +0100

    Improvements on normalisation. Modified the protocol so that more media cna be stored on falcon tubes (up to 45 mL). Now the protocol accounts for dead volume. It will only aspire until 50 microliters are left in the well. Instead of moving the samples to a new well, now all teh dilutions happen on the well where the culture already is.

--- OD_normalization.py
+++ OD_normalization.py
@@ -1,13 +1,16 @@
-od_csv = [[0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
-    [0.36,0.87,1,0-3,0.66,1,0.15,1,0.76,0.4,0.95,0.8]]
+od_csv = [[0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7],
+    [0.75,0.8,0.85,0.9,0.95,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8],
+    [0.36,0.87,1,0.3,0.66,1,0.15,1,0.76,0.4,0.95,0.8]]
 target_od = 0.15
+top_od = 0.2
+initial_volume = 150
 target_volume = 150
+vol_well = 300
 
 metadata = {
     'apiLevel' : '2.11',
@@ -16,47 +19,82 @@ metadata = {
     'description' : 'Produce a 96-well plate with a target OD'
 }
 
-def calculate_OD(initial_OD, final_OD, final_volume):
-    if initial_OD <= final_OD:
-        culture_OD = final_volume - 10
-        media_OD = 10
+def calculate_OD(initial_OD, final_OD, final_volume, start_volume, max_intermediate, max_od):
+    out_vols = []
+
+    subpart = round((final_OD * final_volume) / initial_OD)
+    aspir_vol = start_volume - subpart
+
+    if aspir_vol == 0:
+        return out_vols
+    
+    if aspir_vol <= start_volume - 50:
+        out_vols.append(aspir_vol)
+        out_vols.append(aspir_vol)
+
     else:
-        subpart = (final_OD * final_volume) // initial_OD
-        media_OD = final_volume - subpart
-        culture_OD = subpart
-    return culture_OD, media_OD
+        first_aspir = start_volume - 50
+        out_vols.append(first_aspir)
+        volume_need = round((50 * initial_OD) / final_OD)
+
+        if volume_need <= max_intermediate:
+            media_vol = volume_need - 50
+            out_vols.append(media_vol)
+            well_out = volume_need - final_volume
+            out_vols.append(well_out)
+        else:
+            media_vol = max_intermediate - 50
+            out_vols.append(media_vol)
+            new_OD = (50 * initial_OD) / max_intermediate
+            if new_OD < max_od:
+                out_vols.append(max_intermediate - final_volume)
+            else:
+                second_vols = calculate_OD(new_OD, final_OD, final_volume, max_intermediate, max_intermediate, max_od)
+                for volume in second_vols:
+                    out_vols.append(volume)
+    return out_vols
 
 def run(protocol):
-    reservoir = protocol.load_labware('opentrons_6_tuberack_falcon_50ml_conical', 5)
-    source = reservoir.wells()[0]
+    reservoir = protocol.load_labware('opentrons_6_tuberack_falcon_50ml_conical', 6)
+    source = reservoir.wells()[0].top(z=-36)
+    tr_tube = reservoir.wells()[5].top(z=-14)
 
     tiprack_1 = protocol.load_labware('opentrons_96_tiprack_300ul', 1)
     tiprack_2 = protocol.load_labware('opentrons_96_tiprack_300ul', 4)
+    tiprack_3 = protocol.load_labware('opentrons_96_tiprack_300ul', 3)
+    tiprack_4 = protocol.load_labware('opentrons_96_tiprack_300ul', 5)
 
-    wellplate_0 = protocol.load_labware('corning_96_wellplate_360ul_flat', 2)
-    wellplate_1 = protocol.load_labware('corning_96_wellplate_360ul_flat', 3)
+    wellplate = protocol.load_labware('corning_96_wellplate_360ul_flat', 2)
 
-    l_pip = protocol.load_instrument('p300_single_gen2', 'left', [tiprack_1, tiprack_2])
+    l_pip = protocol.load_instrument('p300_single', 'left', [tiprack_1, tiprack_2, tiprack_3, tiprack_4])
 
-    wp0_wells = []
-    wp1_wells = []
+    source_dirs = []
+    dest_dirs = []
 
-    for row in wellplate_0.rows():
-        wp0_wells += row
-
-    for row in wellplate_1.rows():
-        wp1_wells += row
-
-    culture_vols = []
+    wp_rows = wellplate.rows()
+    vols_transfer = []
     media_vols = []
 
-    for row in od_csv:
-        for col in row:
-            local_culture, local_media = calculate_OD(col, target_od, target_volume)
-            culture_vols.append(local_culture)
-            media_vols.append(local_media)
+    z_pos = -36
+    used_vol = 0
+    max_vol = 5000
+    for row_num in range(len(od_csv)):
+        if used_vol >= max_vol:
+            z_pos -= 9
+            max_vol += 5000
+            source = reservoir.wells()[0].top(z=z_pos)
+        for col_num in range(len(od_csv[row_num])):
+            volumes = calculate_OD(od_csv[row_num][col_num], target_od, target_volume, initial_volume, vol_well, top_od)
+            for vol_num in range(len(volumes)):
+                used_vol += volumes[vol_num]
+                vols_transfer.append(volumes[vol_num])
+                if vol_num%2 == 0:
+                    source_dirs.append(wp_rows[row_num][col_num])
+                    dest_dirs.append(tr_tube)
+                else:
+                    source_dirs.append(source)
+                    dest_dirs.append(wp_rows[row_num][col_num])
 
-    mix_volume = target_volume // 3
+    mix_volume = round(target_volume / 3)
 
-    l_pip.transfer(media_vols, source, wp1_wells, new_tip='once')
-    l_pip.transfer(culture_vols, wp0_wells, wp1_wells, new_tip='always', mix_after=(3,mix_volume))
+    l_pip.transfer(vols_transfer, source_dirs, dest_dirs, new_tip='always', mix_before=(3,mix_volume))
